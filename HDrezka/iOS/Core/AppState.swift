24e878a7556c8f81b974e1ec7efaebb34e8472b2
voidboost <voidboost.hdrezka@gmail.com>
2025-10-10 18:23:31 +0300

    ios (test)

---
+++ HDrezka/iOS/Core/AppState.swift
@@ -0,0 +1,179 @@
+import SwiftUI
+
+@Observable
+class AppState {
+    @ObservationIgnored static let shared = AppState()
+
+    var isSignInPresented = false
+    var isSignUpPresented = false
+    var isRestorePresented = false
+    var isSignOutPresented = false
+
+    var commentsRulesPresented = false
+
+    var isPremiumPresented = false
+
+    var selectedTab: Tabs = .home
+}
+
+enum Tabs: Hashable, Identifiable, CaseIterable {
+    case home
+    case search
+    case categories
+    case collections
+    case watchingLater
+    case bookmarks
+
+    var label: LocalizedStringKey {
+        switch self {
+        case .home:
+            "key.home"
+        case .search:
+            "key.search"
+        case .categories:
+            "key.categories"
+        case .collections:
+            "key.collections"
+        case .watchingLater:
+            "key.watching_later"
+        case .bookmarks:
+            "key.bookmarks"
+        }
+    }
+
+    var image: String {
+        switch self {
+        case .home:
+            "house"
+        case .search:
+            "magnifyingglass"
+        case .categories:
+            "square.grid.2x2"
+        case .collections:
+            "film.stack"
+        case .watchingLater:
+            "clock.arrow.circlepath"
+        case .bookmarks:
+            "bookmark"
+        }
+    }
+
+    var needAccount: Bool {
+        switch self {
+        case .watchingLater, .bookmarks:
+            true
+        default:
+            false
+        }
+    }
+
+    var role: TabRole? {
+        switch self {
+        case .search:
+            .search
+        default:
+            nil
+        }
+    }
+
+    @ViewBuilder
+    func content() -> some View {
+        NavigationStack {
+            switch self {
+            case .home:
+                HomeView()
+                    .id("home")
+                    .destinations()
+            case .search:
+                SearchView()
+                    .id("search")
+                    .destinations()
+            case .categories:
+                CategoriesView()
+                    .id("categories")
+                    .destinations()
+            case .collections:
+                CollectionsView()
+                    .id("collections")
+                    .destinations()
+            case .watchingLater:
+                WatchingLaterView()
+                    .id("watching_later")
+                    .destinations()
+            case .bookmarks:
+                BookmarksView()
+                    .id("bookmarks")
+                    .destinations()
+            }
+        }
+    }
+
+    var id: Self { self }
+}
+
+enum Destinations: Hashable, Identifiable {
+    case details(MovieSimple)
+    case country(MovieCountry)
+    case category(Categories)
+    case genre(MovieGenre)
+    case collection(MoviesCollection)
+    case customList([MovieSimple], String)
+    case list(MovieList)
+    case person(PersonSimple)
+    case comments(MovieDetailed)
+
+    static func fromNamed(_ item: some Named) -> Destinations {
+        if let country = item as? MovieCountry {
+            return .country(country)
+        } else if let genre = item as? MovieGenre {
+            return .genre(genre)
+        } else if let person = item as? PersonSimple {
+            return .person(person)
+        } else if let list = item as? MovieList {
+            return .list(list)
+        } else if let collection = item as? MoviesCollection {
+            return .collection(collection)
+        }
+
+        fatalError("Need \"named\" implementation")
+    }
+
+    var id: Self { self }
+}
+
+extension View {
+    @ViewBuilder
+    func destinations() -> some View {
+        navigationDestination(for: Destinations.self) { destination in
+            switch destination {
+            case let .details(movie):
+                DetailsView(movie: movie)
+                    .id(movie.movieId)
+            case let .country(country):
+                ListView(country: country)
+                    .id(country.countryId)
+            case let .category(category):
+                ListView(category: category)
+                    .id(category)
+            case let .genre(genre):
+                ListView(genre: genre)
+                    .id(genre.genreId)
+            case let .collection(collection):
+                ListView(collection: collection)
+                    .id(collection.collectionId)
+            case let .customList(movies, title):
+                ListView(movies: movies, title: title)
+                    .id(title)
+            case let .list(list):
+                ListView(list: list)
+                    .id(list.listId)
+            case let .person(person):
+                PersonView(person: person)
+                    .id(person.personId)
+            case let .comments(details):
+                CommentsView(details: details)
+                    .id(details.movieId)
+            }
+        }
+    }
+}
---
+++ HDrezka/iOS/HDrezkaApp.swift
@@ -0,0 +1,108 @@
+import Combine
+import Defaults
+import FirebaseAnalytics
+import FirebaseCore
+import FirebaseCrashlytics
+import SwiftData
+import SwiftUI
+import UserNotifications
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+
+        UserDefaults.standard.register(
+            defaults: ["NSApplicationCrashOnExceptions": true],
+        )
+
+        FirebaseApp.configure()
+
+        #if DEBUG
+            Crashlytics.crashlytics().setCrashlyticsCollectionEnabled(false)
+            FirebaseConfiguration.shared.setLoggerLevel(.min)
+            Analytics.setAnalyticsCollectionEnabled(false)
+        #endif
+
+        return true
+    }
+
+//    func applicationWillTerminate(_: Notification) {
+//        if !Downloader.shared.downloads.isEmpty {
+//            let notificationCenter = UNUserNotificationCenter.current()
+//
+//            notificationCenter.getPendingNotificationRequests { requests in
+//                notificationCenter.removePendingNotificationRequests(withIdentifiers: requests.filter { $0.content.categoryIdentifier == "cancel" }.map(\.identifier))
+//            }
+//
+//            notificationCenter.getDeliveredNotifications { notifications in
+//                notificationCenter.removeDeliveredNotifications(withIdentifiers: notifications.filter { $0.request.content.categoryIdentifier == "cancel" }.map(\.request.identifier))
+//            }
+//        }
+//
+//        Downloader.shared.terminate()
+//    }
+//
+//    func application(_: NSApplication, willEncodeRestorableState _: NSCoder) {}
+//
+//    func application(_: NSApplication, didDecodeRestorableState _: NSCoder) {}
+
+    func userNotificationCenter(_: UNUserNotificationCenter, willPresent _: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner, .sound, .badge])
+    }
+//
+//    func userNotificationCenter(_: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+//        let userInfo = response.notification.request.content.userInfo
+//
+//        switch response.actionIdentifier {
+//        case "cancel":
+//            if let gid = userInfo["gid"] as? String {
+//                Downloader.shared.remove(gid)
+//            }
+//        case "open":
+//            if let url = userInfo["url"] as? String, let fileUrl = URL(string: url), fileUrl.isFileURL {
+//                NSWorkspace.shared.activateFileViewerSelecting([fileUrl])
+//            }
+//        case "retry":
+//            if let retryData = userInfo["data"] as? Data, let data = try? JSONDecoder().decode(DownloadData.self, from: retryData) {
+//                Downloader.shared.download(data)
+//            }
+//        case "need_premium":
+//            NSWorkspace.shared.open((Defaults[.mirror] != Defaults.Keys.mirror.defaultValue ? Defaults[.mirror] : Const.redirectMirror).appending(path: "payments", directoryHint: .notDirectory))
+//        default:
+//            break
+//        }
+//
+//        completionHandler()
+//    }
+}
+
+@main
+struct HDrezkaApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var delegate
+    @State private var appState: AppState = .shared
+
+    @State private var modelContainer: ModelContainer
+
+    @Default(.theme) private var theme
+
+    init() {
+        do {
+            let schema = Schema([PlayerPosition.self, SelectPosition.self])
+            let modelContainer = try ModelContainer(for: schema)
+            modelContainer.mainContext.autosaveEnabled = true
+            self.modelContainer = modelContainer
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appState)
+                .preferredColorScheme(theme.scheme)
+        }
+        .modelContainer(modelContainer)
+        .restorationBehavior(.disabled)
+    }
+}
